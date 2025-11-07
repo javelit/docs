268ca7895bbc374014a08c70979d255b0dd2049f
cyrilou242 <cdecatheu@hey.com>
2025-11-07 11:58:57 +0100

    improve clikability

--- components/blocks/newsEntry.js
+++ components/blocks/newsEntry.js
@@ -30,12 +30,15 @@ const NewsEntry = ({ date, title, text, link, image, target }) => {
         <time className={styles.Date} dateTime="date">
           {niceDate(date)}
         </time>
-        <h4 className={styles.Title}>{title}</h4>
+        <a href={link ? link : "#"}>
+          <h4 className={styles.Title}>{title}</h4>
+        </a>
         <p className={styles.Text}>{text}</p>
         <ArrowLink
           link={link ? link : "#"}
           type="next"
           clean={true}
+          noprefix={true}
           className="tiny bold"
           content="Read More"
           target={target ? target : ArrowLink.target}
--- components/navigation/arrowLink.js
+++ components/navigation/arrowLink.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import styles from "./arrowLink.module.css";
 
-const ArrowLink = ({ children, link, type, content, target }) => {
+const ArrowLink = ({ children, link, type, content, target, noprefix }) => {
   function ArrowType() {
     const href = link || "#";
     if (type == "back") {
@@ -29,7 +29,7 @@ const ArrowLink = ({ children, link, type, content, target }) => {
           >
             <path d="M7.22017 13.7159L8.70312 12.2393L4.81037 8.35298H13.9318V6.19247H4.81037L8.70312 2.29972L7.22017 0.829545L0.776989 7.27273L7.22017 13.7159Z" />
           </svg>
-          <span className={styles.Text}>Previous: </span>
+          {!noprefix && <span className={styles.Text}>Previous: </span>}
           <span className={styles.Truncate}>{content}</span>
         </Link>
       );
@@ -45,7 +45,7 @@ const ArrowLink = ({ children, link, type, content, target }) => {
           `}
           target={target == "_blank" ? target : "_self"}
         >
-          <span className={styles.Text}>Next: </span>
+          {!noprefix && <span className={styles.Text}>Next: </span>}
           <span className={styles.Truncate}>{content}</span>
           <svg
             width="14"
